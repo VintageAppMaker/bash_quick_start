26de66ce0a827da92af6d783a9bc721fd44c0f0c
snake <adsloader99@gmail.com>
2018-09-09 17:47:31 +0900

    bash

--- cnvt.sh
+++ cnvt.sh
@@ -2,9 +2,10 @@
 
 for i in *
 do
-   if [ $i == "cnvt.sh" ] 
-   then 
-	   continue 
-   fi
-   expand -i -t 4 $i | sponge $i
+    if [ $i == "cnvt.sh" ] 
+    then 
+        continue 
+    fi
+
+    expand -i -t 4 $i | sponge $i
 done
